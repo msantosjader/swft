2451f9d409ab19229ce4101ddd21cef751d982e0
Jader Santos <msantos.jader@gmail.com>
2025-07-24 13:53:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import smtplib
 import threading
 import time
 
+from functools import wraps
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, send_from_directory
 from flask_cors import CORS
